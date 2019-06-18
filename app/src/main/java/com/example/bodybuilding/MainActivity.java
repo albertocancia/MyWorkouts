3e7cc21ad78b53f98f0fd9fc8ee387a642800e40
Alberto <albertocancia@gmail.com>
2019-06-18 14:49:19 +0200

    Aggiunta SchedaActivity, PastiActivity

--- app/src/main/java/com/example/bodybuilding/MainActivity.java
+++ app/src/main/java/com/example/bodybuilding/MainActivity.java
@@ -2,7 +2,8 @@ package com.example.bodybuilding;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.*;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/bodybuilding/SchedaActivity.java
@@ -0,0 +1,4 @@
+package com.example.bodybuilding;
+
+public class SchedaActivity {
+}
