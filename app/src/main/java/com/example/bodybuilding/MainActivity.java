3d95d05331635e7449f6db93e811248d75f017fa
Gabry <talarico.g7@gmail.com>
2019-06-21 10:59:28 +0200

    Cronometro

---
+++ app/src/main/java/com/example/bodybuilding/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.bodybuilding;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private BottomNavigationView bottomNav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        bottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch(menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_scheda:
+                            selectedFragment = new SchedeFragment();
+                            break;
+                        case R.id.nav_dieta:
+                            selectedFragment = new DietaFragment();
+                            break;
+                        case R.id.nav_timer:
+                            selectedFragment = new TimerFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+}
---
+++ app/src/main/java/com/example/bodybuilding/TimerFragment.java
@@ -0,0 +1,62 @@
+package com.example.bodybuilding;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.Toast;
+
+public class TimerFragment extends Fragment implements View.OnClickListener{
+    private Chronometer chronometer;
+    private long pauseOffset;
+    private boolean running;
+    private Button button_start, button_reset, button_pause;
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_timer, container, false);
+
+        button_start = view.findViewById(R.id.button_start);
+        button_pause = view.findViewById(R.id.button_pause);
+        button_reset = view.findViewById(R.id.button_reset);
+        chronometer = view.findViewById(R.id.chronometer);
+
+        chronometer.setFormat("Time: %s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+
+
+        button_start.setOnClickListener(this);
+        button_pause.setOnClickListener(this);
+        button_reset.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == view.findViewById(R.id.button_start)){
+            if(!running){
+                chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+                chronometer.start();
+                running = true;
+            }
+        }else if(v == view.findViewById(R.id.button_pause)){
+            if(running){
+                chronometer.stop();
+                pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+                running = false;
+            }
+        }else if(v == view.findViewById(R.id.button_reset)){
+            chronometer.setBase(SystemClock.elapsedRealtime());
+            pauseOffset = 0;
+        }
+    }
+}
